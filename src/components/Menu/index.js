0338356da9c32f0e20da0682a80d6abecb715ddf
Guilherme Luchesi <gui.olhenrique@gmail.com>
2017-11-24 09:26:08 -0200

    adiciona smooth scroll

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
+import { Link } from 'react-scroll'
+
 import mainMenu from '../../config/menu.js'
 
 import './menu.css'
 
-const agrupaItens = item =>
-  (<li key={item.titulo}><a href={item.url}>{item.titulo}</a></li>)
+const agrupaItens = item => (
+  <li key={item.titulo}>
+    <Link to={item.url} smooth={true}>{item.titulo}</Link>
+  </li>
+)
 
 const Menu = () => (mainMenu)
   ? (<ul className='mainMenu'>{mainMenu.map(agrupaItens)}</ul>)
